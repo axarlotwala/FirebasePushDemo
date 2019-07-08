375fd1375fe05e229994a01c2b3186f759fb1ab6
axar lotwala <axarlotwala@gmail.com>
2019-07-08 22:52:08 +0530

    upcoming push demo with realtime status update

---
+++ PushStatus/app/src/main/java/com/example/pushstatus/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.pushstatus;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+
+                        try{
+
+                            String token = task.getResult().getToken();
+                            Log.e("Fcm_Token",""+token);
+
+                        }catch (Exception e){
+                            Global.Toasty(e.getMessage(),getApplicationContext());
+                        }
+
+                    }
+                });
+    }
+}
